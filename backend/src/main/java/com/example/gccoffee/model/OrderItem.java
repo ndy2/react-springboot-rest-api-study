ca4f07dcacf62b5505295a36e125e25abb01372c
ndy2 <emrdbs12@gmail.com>
2022-04-27 21:12:21 +0900

    Day3 Lec1
    OrderItem, OrderJdbcRepository

--- backend/src/main/java/com/example/gccoffee/model/OrderItem.java
+++ backend/src/main/java/com/example/gccoffee/model/OrderItem.java
@@ -1,4 +1,33 @@
 package com.example.gccoffee.model;
 
+import java.util.UUID;
+
 public class OrderItem {
+    private UUID productId;
+    private Category category;
+    private long price;
+    private long quantity;
+
+    public OrderItem(UUID productId, Category category, long price, long quantity) {
+        this.productId = productId;
+        this.category = category;
+        this.price = price;
+        this.quantity = quantity;
+    }
+
+    public UUID getProductId() {
+        return productId;
+    }
+
+    public Category getCategory() {
+        return category;
+    }
+
+    public long getPrice() {
+        return price;
+    }
+
+    public long getQuantity() {
+        return quantity;
+    }
 }
